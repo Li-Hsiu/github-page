720332da094b0624232e312f87b2c69bcf34599e
HSU, Li-hsiu <lhsuaa@connect.ust.hk>
2024-02-05 00:09:26 +0800

    imported assets 1

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/examples/jsm/webxr/VRButton.js'; 
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-//import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-let scene, camera, renderer, cube;
+let scene, camera, renderer, cube, floor, loadedModel;
 
 init();
 animate()
@@ -13,10 +13,10 @@ function init() {
     scene.background = new THREE.Color('skyblue');
 
     camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 100); 
-    camera.lookAt(0, 0, 0);
-    camera.position.set(0,0,40);
+    camera.lookAt(0, 0, 5);
+    camera.position.set(0,1.6,0);
 
-    renderer = new THREE.WebGLRenderer(); 
+    renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     renderer.setPixelRatio(window.devicePixelRatio); 
 
@@ -25,10 +25,43 @@ function init() {
     document.body.appendChild(renderer.domElement);
     document.body.appendChild(VRButton.createButton(renderer));
 
-    const geometryCube = new THREE.BoxGeometry(2, 2, 2); 
-    const materialCube = new THREE.MeshBasicMaterial( { color: 0xFFC0CB } ); 
+    const directionalLight = new THREE.AmbientLight(0xFFFFFF, 1.2);
+    //directionalLight.position.set(0, 5, 0);
+    scene.add(directionalLight);
+    
+    /*const geometryCube = new THREE.BoxGeometry(2, 2, 2); 
+    const materialCube = new THREE.MeshPhongMaterial( { color: 0xFFC0CB } ); 
     cube = new THREE.Mesh(geometryCube, materialCube); 
-    scene.add(cube);
+    cube.position.set(0, 0, 20);
+    scene.add(cube);*/
+
+    const geometryCube = new THREE.BoxGeometry(100, 1, 100); 
+    const materialCube = new THREE.MeshPhongMaterial( { color: 0xD7C8AC } ); 
+    floor = new THREE.Mesh(geometryCube, materialCube); 
+    floor.position.set(0, -1, 0);
+    scene.add(floor);
+
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('./assets/sci_fi_base/scene.gltf', (gltfScene) => {
+        gltfScene.scene.traverse(c => {
+            c.castShadow = true;
+        });
+        gltfScene.scene.rotation.y = Math.PI/2;
+        gltfScene.scene.position.set(0,0,20);
+        gltfScene.scene.scale.set(1, 1, 1);
+        scene.add(gltfScene.scene);
+    });
+
+    gltfLoader.load('./assets/mushroom/scene.gltf', (gltfScene) => {
+        loadedModel = gltfScene;
+        gltfScene.scene.traverse(c => {
+            c.castShadow = true;
+        });
+        gltfScene.scene.rotation.y = Math.PI/2;
+        gltfScene.scene.position.set(0,-0.43,10);
+        gltfScene.scene.scale.set(0.75, 0.75, 0.75);
+        scene.add(gltfScene.scene);
+    });
 
     window.addEventListener('resize', onWindowResize);
 }
@@ -39,8 +72,9 @@ function animate() {
 }
 
 function render() {
-    cube.rotation.x += 0.01; 
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01; 
+    //cube.rotation.y += 0.01;
+    loadedModel.scene.rotation.y += 0.005;
     renderer.render(scene, camera);
 }
 function onWindowResize() {
